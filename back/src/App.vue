c55b2a3111deb52c090e3824846b59b68f6ec4d7
wangpenbin <2063870013@qq.com>
2019-02-28 17:13:32 +0800

    添加了一些文件

---
+++ back/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+  	<app-quote @add="getDate($event)"></app-quote>
+   		<app-quote-gird :quotes="quotes"></app-quote-gird>
+   		<button @click="quoted"></button>
+   		
+  </div>
+</template>
+
+<script>
+	import quoteird from "./conponets/quotegirde.vue"
+	import quote from "./conponets/quote.vue"
+export default {
+	
+	data:function(){
+		
+ 	return {
+ 		maxlength:10,
+ 		quotes:[
+ 			"this is a first "
+ 		],
+ 	}
+ },
+ components :{
+ 	appQuoteGird:quoteird,
+ 	appQuote :quote
+ },
+ methods:{
+ 	getData:function(data){
+ 		this.quotes.push(data);
+ 	}
+ }
+}
+</script>
+
+<style>
+
+</style>
